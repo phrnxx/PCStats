0df1e96935b37f700a8e467afee659859f454f51
1 <1@DESKTOP-DALMISK>
2024-10-15 11:58:12 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace PCStatsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label17 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.SystemColors.InfoText;
+            this.label17.ForeColor = System.Drawing.SystemColors.Control;
+            this.label17.Location = new System.Drawing.Point(17, 715);
+            this.label17.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(41, 16);
+            this.label17.TabIndex = 26;
+            this.label17.Text = "label17";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label11.Location = new System.Drawing.Point(6, 38);
+            this.label11.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label11.Name = "label11";
+            this.label11.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label11.Size = new System.Drawing.Size(39, 16);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label12.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label12.Location = new System.Drawing.Point(6, 54);
+            this.label12.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label12.Name = "label12";
+            this.label12.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label12.Size = new System.Drawing.Size(41, 16);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label13.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label13.Location = new System.Drawing.Point(6, 21);
+            this.label13.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label13.Name = "label13";
+            this.label13.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label13.Size = new System.Drawing.Size(41, 16);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "label13";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(6, 53);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label6.Size = new System.Drawing.Size(37, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(6, 21);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label7.Size = new System.Drawing.Size(37, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox3.Controls.Add(this.label16);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.label13);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox3.Location = new System.Drawing.Point(6, 171);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox3.Size = new System.Drawing.Size(294, 75);
+            this.groupBox3.TabIndex = 21;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label16.Location = new System.Drawing.Point(252, 54);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(42, 16);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "by.frnx";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label10.Location = new System.Drawing.Point(6, 37);
+            this.label10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label10.Name = "label10";
+            this.label10.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label10.Size = new System.Drawing.Size(41, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "label10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(65, 715);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(6, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.InfoText;
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(103, 715);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 16);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "label5";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox2.Location = new System.Drawing.Point(6, 85);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox2.Size = new System.Drawing.Size(294, 76);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(6, 5);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(294, 70);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.Transparent;
+            this.label14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label14.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label14.Location = new System.Drawing.Point(6, 16);
+            this.label14.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label14.Name = "label14";
+            this.label14.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label14.Size = new System.Drawing.Size(42, 16);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "label14";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(305, 248);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "MainForm";
+            this.Text = "PCStats";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Management;
+using OpenHardwareMonitor.Hardware;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace PCStatsApp
+{
+    public partial class MainForm : Form
+    {
+       
+        private readonly Computer computer;
+        private Thread updateThread;
+        private bool isFormClosing = false;
+        private string processorName = "";
+        private string gpuName = "";
+        private string motherboardName = "";
+       
+
+        public MainForm()
+        {
+            InitializeComponent();
+           
+
+            computer = new Computer();
+            computer.Open();
+
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.BackColor = Color.Black;
+            this.ForeColor = Color.White;
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+
+            computer.CPUEnabled = true;
+            computer.GPUEnabled = true;
+            computer.MainboardEnabled = true;
+
+            updateThread = new Thread(UpdateSystemInfo);
+            updateThread.IsBackground = true;
+            updateThread.Start();
+
+            groupBox1.Text = "Процессор";
+            groupBox2.Text = "Видеокарта";
+            groupBox3.Text = "Память";
+
+
+            this.FormClosing += MainForm_FormClosing;
+        }
+
+       
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isFormClosing = true;
+            if (updateThread != null && updateThread.IsAlive)
+            {
+                updateThread.Interrupt();
+                if (!updateThread.Join(600)) // Обновление каждие 6с 
+                {
+                    updateThread.Abort(); // Принудительное завершение потока
+                }
+            }
+            computer.Close();
+        }
+
+        private void UpdateSystemInfo()
+        {
+            try
+            {
+                while (!isFormClosing)
+                {
+                    UpdateProcessorInfo();
+                    UpdateGPUInfo();
+                    UpdateMemoryInfo();
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                // Поток был прерван, выйти из метода
+            }
+        }
+
+
+        //Processor
+        private void UpdateProcessorInfo()
+        {
+            foreach (var hardware in computer.Hardware)
+            {
+                if (hardware.HardwareType == HardwareType.CPU)
+                {
+                    hardware.Update();
+                    processorName = hardware.Name;
+                    string processorBrand = "";
+                    string processorModel = "";
+                    int numberOfCores = 0;
+                    float processorLoad = 0;
+                    float processorTemperature = 0;
+                    int loadSensorsCount = 0;
+
+                    if (processorName.Contains("Intel"))
+                    {
+                        processorBrand = "Intel";
+                        processorModel = processorName.Replace("Intel ", "").Trim();
+                    }
+                    else if (processorName.Contains("AMD"))
+                    {
+                        processorBrand = "AMD";
+                        int startIndex = processorName.IndexOf("AMD ") + 4;
+                        processorModel = processorName.Substring(startIndex);
+                    }
+
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            if (sensor.Name.ToLower().Contains("core"))
+                            {
+                                processorTemperature += sensor.Value.GetValueOrDefault();
+                                numberOfCores++;
+                            }
+                            else if (processorBrand == "AMD" && sensor.Name.ToLower().Contains("package"))
+                            {
+                                processorTemperature += sensor.Value.GetValueOrDefault();
+                            }
+                        }
+                        else if (sensor.SensorType == SensorType.Load && sensor.Name.ToLower().Contains("total"))
+                        {
+                            processorLoad += sensor.Value.GetValueOrDefault();
+                            loadSensorsCount++;
+                        }
+                    }
+
+                    if (numberOfCores > 0)
+                        processorTemperature /= numberOfCores;
+                    else if (processorBrand == "AMD")
+                        processorTemperature /= 1; 
+
+                    if (loadSensorsCount > 0)
+                        processorLoad /= loadSensorsCount;
+
+                    int roundedTemperature = (int)Math.Round(processorTemperature, 0);
+                    int roundedLoad = (int)Math.Round(processorLoad, 0);
+
+                    if (this.InvokeRequired)
+                    {
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            UpdateProcessorInfoUI(processorName, processorBrand, processorModel, numberOfCores, roundedLoad, roundedTemperature);
+                        });
+                    }
+                    else
+                    {
+                        UpdateProcessorInfoUI(processorName, processorBrand, processorModel, numberOfCores, roundedLoad, roundedTemperature);
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        private void UpdateProcessorInfoUI(string processorName, string processorBrand, string processorModel, int numberOfCores, int roundedLoad, int roundedTemperature)
+        {
+            label14.Text = $"{processorBrand} {processorModel}";
+            label1.Text = $"Количество ядер: {numberOfCores}";
+            label2.Text = $"Загрузка процессора: {roundedLoad}%";
+            label3.Text = $"Температура процессора: {roundedTemperature}°C";
+        }
+
+        //GPU
+        private void UpdateGPUInfo()
+        {
+            foreach (var hardware in computer.Hardware)
+            {
+                if (hardware.HardwareType == HardwareType.GpuNvidia)
+                {
+                    hardware.Update();
+
+                    gpuName = hardware.Name;
+                    float gpuTemperature = 0;
+                    float gpuLoad = 0;
+                    float gpuMemoryUsage = 0;
+                    float gpuVideoEngineLoad = 0;
+                    float gpuBusInterfaceLoad = 0;
+
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            gpuTemperature = sensor.Value.GetValueOrDefault();
+                        }
+                        else if (sensor.SensorType == SensorType.Load && sensor.Name.ToLower().Contains("gpu core"))
+                        {
+                            gpuLoad = sensor.Value.GetValueOrDefault();
+                        }
+                        else if (sensor.SensorType == SensorType.Load && sensor.Name.ToLower().Contains("memory controller"))
+                        {
+                            gpuMemoryUsage = sensor.Value.GetValueOrDefault();
+                        }
+                        else if (sensor.SensorType == SensorType.Load && sensor.Name.ToLower().Contains("video engine"))
+                        {
+                            gpuVideoEngineLoad = sensor.Value.GetValueOrDefault();
+                        }
+                        else if (sensor.SensorType == SensorType.Load && sensor.Name.ToLower().Contains("bus interface"))
+                        {
+                            gpuBusInterfaceLoad = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+
+                    if (this.InvokeRequired)
+                    {
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            UpdateGPUInfoUI(gpuName, gpuTemperature, gpuLoad, gpuMemoryUsage, gpuVideoEngineLoad, gpuBusInterfaceLoad);
+                        });
+                    }
+                    else
+                    {
+                        UpdateGPUInfoUI(gpuName, gpuTemperature, gpuLoad, gpuMemoryUsage, gpuVideoEngineLoad, gpuBusInterfaceLoad);
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        private void UpdateGPUInfoUI(string gpuName, float gpuTemperature, float gpuLoad, float gpuMemoryUsage, float gpuVideoEngineLoad, float gpuBusInterfaceLoad)
+        {
+            groupBox2.Text = $"Видеокарта: {gpuName}";
+            label6.Text = $"Температура GPU: {gpuTemperature}°C";
+            label7.Text = $"Загрузка GPU: {gpuLoad}%";
+            label10.Text = $"Загрузка шины GPU: {gpuBusInterfaceLoad}%";
+        }
+
+        //RAM
+        private void UpdateMemoryInfo()
+        {
+            ObjectQuery queryRam = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcherRam = new ManagementObjectSearcher(queryRam);
+            foreach (ManagementObject obj in searcherRam.Get())
+            {
+                double totalMemoryGB = Math.Round(Convert.ToDouble(obj["TotalVisibleMemorySize"]) / 1024.0 / 1024.0, 2);
+                double freeMemoryGB = Math.Round(Convert.ToDouble(obj["FreePhysicalMemory"]) / 1024.0 / 1024.0, 2);
+                double memoryLoad = 100 - Math.Round((freeMemoryGB / totalMemoryGB) * 100);
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    label11.Text = $"Общая память: {totalMemoryGB} GB";
+                    label12.Text = $"Свободная память: {freeMemoryGB} GB";
+                    label13.Text = $"Загрузка памяти: {memoryLoad}%";
+                });
+
+                break;
+            }
+        }
+    }
+}
